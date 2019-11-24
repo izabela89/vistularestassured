3eea3b4a43d63bf336b78be8fa677ff9e84d0235
iza_gol <izabela.golawska89@gmail.com>
2019-11-24 16:48:29 +0100

    zajęcia 24.11

--- src/test/java/org/vistula/restassured/information/InformationControllerTest.java
+++ src/test/java/org/vistula/restassured/information/InformationControllerTest.java
@@ -1,10 +1,14 @@
 package org.vistula.restassured.information;
 
+import org.apache.commons.lang3.RandomStringUtils;
+import org.json.JSONObject;
 import org.junit.Test;
 import org.vistula.restassured.RestAssuredTest;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.number.OrderingComparison.greaterThan;
 
 public class InformationControllerTest extends RestAssuredTest {
 
@@ -16,5 +20,36 @@ public class InformationControllerTest extends RestAssuredTest {
                 .statusCode(200)
                 .body("size()", is(2));
     }
+    @Test
+    public void shouldcreateNewPlayer() {
+        JSONObject requestParams = new JSONObject();
+        String myName = RandomStringUtils.randomAlphabetic(17);
+        String nationality = "Polska";
+        int mySalary = 100000000;
 
+        requestParams.put ("name", myName);
+                requestParams.put("nationality", nationality);
+                requestParams.put("salary", mySalary);
+        requestParams.put ("id", 4);
+
+        given().header("Content-Type", "application/json")
+                .body(requestParams.toString())
+                .post("/information/")
+                .then()
+                .log().all()
+                .statusCode(201)
+
+                .body("nationality", equalTo(nationality))
+                .body ("salary", equalTo (mySalary))
+                .body("name", equalTo (myName))
+                .body("id", greaterThan (0));
+
+    }
+    @Test
+    public void shouldDeleteInfo(){
+        given().delete("/information/2")
+                .then()
+                .log().all()
+                .statusCode(204);
+    }
 }
