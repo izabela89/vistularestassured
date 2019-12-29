6dc98c486e8c4e9559fb0fa150e43139d7266c97
iza_gol <izabela.golawska89@gmail.com>
2019-12-29 20:19:58 +0100

    praca domowa

--- src/test/java/org/vistula/restassured/information/InformationControllerTest.java
+++ src/test/java/org/vistula/restassured/information/InformationControllerTest.java
@@ -1,6 +1,7 @@
 package org.vistula.restassured.information;
 
 import org.apache.commons.lang3.RandomStringUtils;
+import org.hamcrest.CoreMatchers;
 import org.json.JSONObject;
 import org.junit.Test;
 import org.vistula.restassured.RestAssuredTest;
@@ -12,44 +13,62 @@ import static org.hamcrest.number.OrderingComparison.greaterThan;
 
 public class InformationControllerTest extends RestAssuredTest {
 
+
     @Test
     public void shouldGetAll() {
         given().get("/information")
                 .then()
                 .log().all()
                 .statusCode(200)
-                .body("size()", is(2));
+                .body("size()", is(5));
     }
-    @Test
-    public void shouldcreateNewPlayer() {
-        JSONObject requestParams = new JSONObject();
-        String myName = RandomStringUtils.randomAlphabetic(17);
-        String nationality = "Polska";
-        int mySalary = 100000000;
 
-        requestParams.put ("name", myName);
-                requestParams.put("nationality", nationality);
-                requestParams.put("salary", mySalary);
-        requestParams.put ("id", 4);
+    @Test
+    public void shouldPostData() {
+
+        JSONObject update = new JSONObject();
+        int _id = 10;
+        String _nam = "Iza";
+        String _nat = "Polska";
+        int _sal = 1000;
+
+        update.put("id", _id);
+        update.put("name", _nam);
+        update.put("nationality", _nat);
+        update.put("salary", _sal);
 
         given().header("Content-Type", "application/json")
-                .body(requestParams.toString())
-                .post("/information/")
+                .body(update.toString())
+                .post("/information")
                 .then()
                 .log().all()
                 .statusCode(201)
 
-                .body("nationality", equalTo(nationality))
-                .body ("salary", equalTo (mySalary))
-                .body("name", equalTo (myName))
-                .body("id", greaterThan (0));
-
+                .body("name", CoreMatchers.equalTo(_nam))
+                .body("nationality", CoreMatchers.equalTo(_nat))
+                .body("salary", CoreMatchers.equalTo(_sal))
+                .body("id", greaterThan(9));
     }
+
+
     @Test
-    public void shouldDeleteInfo(){
-        given().delete("/information/2")
+    public void shouldPatchData() {
+
+        JSONObject update = new JSONObject();
+        String _nam = "Iza";
+        int _id = 10;
+
+        update.put("id", _id);
+        update.put("name", _nam);
+
+        given().header("Content-Type", "application/json")
+                .body(update.toString())
+                .patch("/information/10")
                 .then()
                 .log().all()
-                .statusCode(204);
+                .statusCode(200)
+                .body("name", CoreMatchers.equalTo(_nam))
+                .body("id", greaterThan(0));
     }
+
 }
